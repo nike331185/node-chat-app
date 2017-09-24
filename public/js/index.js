e6294083ef40975d8ee100649b98b24e7987975d
howard <nike331185@gmail.com>
2017-09-24 14:21:23 +0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 socket.on('connect',() => {
     console.log('Connected to server');
 
-    socket.emit('createMessage',{
-        from: '123@example.com',
-        text:'123',
-        createAt:123
-    });
-
 });
 socket.on('disconnect',() => {
     console.log('Disconnected from server');
